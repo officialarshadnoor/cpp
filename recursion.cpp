ecc0be87317dca7f9e67660e0189bd98d3bcf806
Md Arshad Noor - 12204331 <115112430+officialarshadnoor@users.noreply.github.com>
2023-09-14 03:52:34 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -180,26 +180,163 @@
 
 // Palindrome Check using recursion method
 
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// bool isPalindrome(string str, int start, int end)
+// {
+//     if (start >= end)
+//         return true;
+//     if (str[start] != str[end])
+//         return false;
+//     return isPalindrome(str, start + 1, end - 1);
+// }
+
+// int main()
+// {
+//     string input;
+//     cout << "Enter a string: ";
+//     cin >> input;
+//     if (isPalindrome(input, 0, input.length() - 1))
+//         cout << input << " is a pelindrome. " << endl;
+//     else
+//         cout << input << " is not a pelindrome." << endl;
+//     return 0;
+// }
+
+// Take input from user and count the number of entered number
+// #include <iostream>
+// using namespace std;
+// int countNum(int n)
+// {
+//     int count = 0;
+//     while (n != 0)
+//     {
+//         n = n / 10;
+//         ++count;
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     cout << "Enter the number: ";
+//     int n;
+//     cin >> n;
+//     cout << "No of digits: " << countNum(n);
+// }
+
+// #include <iostream>
+// #include <cstring>
+// #include <string>
+// using namespace std;
+// int main()
+// {
+//     string str{"Steve jobs"};
+//     char *cstr = new char[str.length() + 1];
+//     strcpy(cstr, str.c_str());
+//     char *p = strtok(cstr, " ");
+//     while (p != 0)
+//     {
+//         cout << p << "\n";
+//         p = strtok(NULL, " ");
+//     }
+//     delete[] cstr;
+//     return 0;
+// }
+
+// Day 2 of Resursion
+// #include <iostream>
+// using namespace std;
+
+// void print(int arr[], int n, int i)
+// {
+//     // base case
+//     if (i >= n)
+//     {
+//         return;
+//     }
+
+//     // 1 case solved
+//     cout << arr[i] << " ";
+
+//     print(arr, n, i + 1);
+// }
+
+// int main()
+// {
+//     int arr[5] = {10, 20, 30, 40, 50};
+//     int n = 5;
+//     int i = 0;
+
+//     print(arr, n, i);
+// }
+
+// Find maximum value using recursion method
+// #include <iostream>
+// #include <limits.h>
+// using namespace std;
+// void findMax(int arr[], int n, int i, int &maxi)
+// {
+//     // base case
+//     if (i >= n)
+//     {
+//         return;
+//     }
+//     // checking current element for max
+//     if (arr[i] > maxi)
+//     {
+//         maxi = arr[i];
+//     }
+
+//     // left all recursion will do
+//     findMax(arr, n, i + 1, maxi);
+// }
+
+// int main()
+// {
+//     int arr[] = {10, 30, 21, 54, 18, 17, 66, 92};
+//     int n = 8;
+
+//     int maxi = INT_MIN;
+
+//     int i = 0;
+
+//     findMax(arr, n, i, maxi);
+//     cout << "Maximum number is: " << maxi << endl;
+// }
+
+// Character is available in the array or not
 #include <iostream>
-#include <string>
 using namespace std;
-bool isPalindrome(string str, int start, int end)
+
+bool checkKey(string str, int i, int n, char key)
 {
-    if (start >= end)
-        return true;
-    if (str[start] != str[end])
+    // base case
+    if (i >= n)
+    {
         return false;
-    return isPalindrome(str, start + 1, end - 1);
+    }
+    // we'll solve one case
+    if (str[i] == key)
+    {
+        return true;
+    }
+
+    // baki sab recursion sambhal lega
+    return checkKey(str, i + 1, n, key);
 }
 
 int main()
 {
-    string input;
-    cout << "Enter a string: ";
-    cin >> input;
-    if (isPalindrome(input, 0, input.length() - 1))
-        cout << input << " is a pelindrome. " << endl;
-    else
-        cout << input << " is not a pelindrome." << endl;
+    string str = "ArshadNoor";
+    int n = str.length();
+
+    char key = 'r';
+
+    int i = 0;
+    bool ans = checkKey(str, i, n, key);
+    cout << "Answer is: " << ans << endl;
+
     return 0;
 }
